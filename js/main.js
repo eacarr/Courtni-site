a1bc31e2cebdecb6d1cc690e36bcc99a4eaac83c
Everette Carrera <everettecarrera@gmail.com>
2022-06-02 12:14:44 -0500

    Add Mobile hamburger menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const toggler = document.querySelector(".toggler");
+const body = document.querySelector("body");
+///////////////////////////////////////
+// Modal window
+
+const openModal = function (e) {
+  // e.preventDefault();
+  if (
+    modal.classList.contains("hidden") &&
+    overlay.classList.contains("hidden") &&
+    !body.classList.contains("noscroll")
+  ) {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+    body.classList.add("noscroll");
+  } else {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+    body.classList.remove("noscroll");
+  }
+};
+
+toggler.addEventListener("click", openModal);
+
 // Items reveal
 const signUp = document.querySelector(".sign_up");
 const reveal = function (entries, observer) {
